392bc78d96e831b3fd148f591ee3608a039a586b
temetvince <temetvince@gmail.com>
2023-03-24 22:10:16 -0500

    Can load image, view squares, move around

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -12,16 +12,58 @@ namespace art_grid.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
+    public MainWindowViewModel()
+    {
+        _startX = 0;
+        _startY = 0;
+    }
+
     public static string ImageTitle => "Open Image";
+    public static string UpTitle => "Up";
+    public static string DownTitle => "Down";
+    public static string LeftTitle => "Left";
+    public static string RightTitle => "Right";
 
     public Avalonia.Media.Imaging.Bitmap? GetImage
     {
         get => _croppedBitmap;
         private set => this.RaiseAndSetIfChanged(ref _croppedBitmap, value);
     }
+    
+    public void Up()
+    {
+        Crop(Direction.Up);
+    }
+
+    public void Down()
+    {
+        Crop(Direction.Down);
+    }
+
+    public void Left()
+    {
+        Crop(Direction.Left);
+    }
+
+    public void Right()
+    {
+        Crop(Direction.Right);
+    }
 
     private Bitmap? _bitmap;
     private Avalonia.Media.Imaging.Bitmap? _croppedBitmap;
+    private int _startX;
+    private int _startY;
+    private const int Cells = 20;
+
+    private enum Direction
+    {
+        Up,
+        Down,
+        Left,
+        Right,
+        None
+    }
 
     private void PickFile()
     {
@@ -35,17 +77,17 @@ public class MainWindowViewModel : ViewModelBase
 
         _bitmap = new Bitmap(File.OpenRead(file));
 
-        Crop();
+        Crop(Direction.None);
     }
 
-    private string? GetFile(IClassicDesktopStyleApplicationLifetime desktop)
+    private static string? GetFile(IClassicDesktopStyleApplicationLifetime desktop)
     {
         var dialog = new OpenFileDialog();
 
         return dialog.ShowAsync(desktop.MainWindow).Result?[0];
     }
 
-    private void Crop()
+    private void Crop(Direction direction)
     {
         if (_bitmap == null)
             return;
@@ -59,11 +101,11 @@ public class MainWindowViewModel : ViewModelBase
         var origBytes = new Byte[origByteCount];
         Marshal.Copy(rawOriginal.Scan0, origBytes, 0, origByteCount);
 
-        //I want to crop a 100x100 section starting at 15, 15.
-        const int startX = 15;
-        const int startY = 15;
-        const int width = 100;
-        const int height = 100;
+        var size = (_bitmap.Width < _bitmap.Height) ? _bitmap.Width / Cells : _bitmap.Height / Cells;
+        _startX = GetX(direction, size);
+        _startY = GetY(direction, size);
+        var width = size;
+        var height = size;
         const int bpp = 4; //4 Bpp = 32 bits, 3 = 24, etc.
 
         var croppedBytes = new Byte[width * height * bpp];
@@ -73,7 +115,7 @@ public class MainWindowViewModel : ViewModelBase
         {
             for (var j = 0; j < width * bpp; j += bpp)
             {
-                var origIndex = (startX * rawOriginal.Stride) + (i * rawOriginal.Stride) + (startY * bpp) + (j);
+                var origIndex = (_startX * rawOriginal.Stride) + (i * rawOriginal.Stride) + (_startY * bpp) + (j);
                 var croppedIndex = (i * width * bpp) + (j);
 
                 //copy data: once for each channel
@@ -98,5 +140,39 @@ public class MainWindowViewModel : ViewModelBase
         memory.Position = 0;
 
         GetImage = new Avalonia.Media.Imaging.Bitmap(memory);
+    }
+
+    private int GetX(Direction direction, int size)
+    {
+        if (_bitmap == null)
+            return 0;
+
+        if (direction == Direction.None)
+            return 0;
+
+        return direction switch
+        {
+            Direction.Left or Direction.Right => _startX,
+            Direction.Up => (_startX - size > 0) ? _startX - size : 0,
+            Direction.Down => (_startX + 2 * size < _bitmap.Height) ? _startX + size : _startX,
+            _ => 0
+        };
+    }
+
+    private int GetY(Direction direction, int size)
+    {
+        if (_bitmap == null)
+            return 0;
+
+        if (direction == Direction.None)
+            return 0;
+
+        return direction switch
+        {
+            Direction.Up or Direction.Down => _startY,
+            Direction.Left => (_startY - size > 0) ? _startY - size : 0,
+            Direction.Right => (_startY + 2 * size < _bitmap.Width) ? _startY + size : _startY,
+            _ => 0
+        };
     }
 }
